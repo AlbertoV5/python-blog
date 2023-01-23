d8d00528df12b66214c610427f76aa1bc342d818
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2023-01-23 14:53:32 -0600

    improvements to blog layout' '

--- astro-app/src/components/navigation/LeftTOC.tsx
+++ astro-app/src/components/navigation/LeftTOC.tsx
@@ -7,29 +7,37 @@ const LeftTOC: React.FC<TOCProps> = ({className, headings}) => {
 
   const name = 'Table of Contents';
   return (
-    <div 
+    <div
       className={className}
       style={{
         position: "sticky", 
-        left: "1em", 
+        left: "1em",
         top: "10vh",
         height: "80vh",
         overflow: "auto",
         zIndex: "100"
       }}
     >
-      <div className="container-fluid">
+      <div className="container">
         <div className="row">
           <h5 className="pt-5 pb-1 px-1">
-            <a href={`#${headings[0].slug}`} className="list-group-item">{name}</a>
+            <a href={`#top`} className="list-group-item">{name}</a>
           </h5>
-          <div className="list-group px-1">
-            {headings.slice(1, undefined).map((h) => (
+          {/* <div className="input-group input-group-sm mb-3 px-1">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+              </svg>
+            </button>
+            <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2"/>
+          </div> */}
+          <div className="list-group px-1 pt-2">
+            {headings.map((h) => (
               <a
                 key={`${h.slug}`}
                 href={`#${h.slug}`} 
                 className="list-group-item"
-                style={{paddingLeft: `${(h.depth-1) * 10}px`, fontSize: "0.9em"}}
+                style={{fontSize: "0.9em"}}
               >
                 {`${h.text}`}
               </a>
--- astro-app/src/components/navigation/TopTOC.tsx
+++ astro-app/src/components/navigation/TopTOC.tsx
@@ -16,17 +16,17 @@ const TopTOC: React.FC<TOCProps> = ({className, headings}) => {
         zIndex: "100"
       }}
     >
-      <div className="container-fluid bg-body border-bottom">
+      <div className="container bg-body border-bottom">
         <div className="d-flex flex-row justify-content-center py-2">
-          <h5 className="py-2 px-2"><a href={`#${headings[0].slug}`} className="list-group-item">{name}</a></h5>
+          <h5 className="py-2 px-2"><a href={`#top`} className="list-group-item">{name}</a></h5>
           <div className="dropdown py-1">
             <button className="btn btn-light dropdown-toggle mx-2" type="button" style={{width: "40px", height:"36px"}} data-bs-toggle="dropdown" aria-expanded="false">
             </button>
             <ul className="dropdown-menu p-2 rounded">
-              {headings.slice(1, undefined).map((h) => (
-                <li>
+              {headings.map((h) => (
+                <li key={`list-${h.slug}`}>
                   <a
-                    key={`${h.slug}`}
+                    key={`anchor-${h.slug}`}
                     href={`#${h.slug}`} 
                     className="dropdown-item border-bottom py-2"
                     style={{fontSize: "1em"}}
